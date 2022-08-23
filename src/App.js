e85a2eab5a43a8ead0ae6365b0e3a7b61921f324
thongphet.k <thongphet.k@kolaogroup.com>
2022-08-23 23:14:33 +0700

    react dashboard

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import IndexDashboard from './views/Dashboard/IndexDashboard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <IndexDashboard/>
+    </div>
+  );
+}
+
+export default App;
